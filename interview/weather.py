b42773a9c9023e35b5827f5b2614242ad6fd14cf
hmzhou1 <137363984+hmzhou1@users.noreply.github.com>
2023-06-22 15:10:18 +0000

    my initial edits

--- interview/weather.py
+++ interview/weather.py
@@ -1,3 +1,52 @@
 
 def process_csv(reader, writer):
+    high = 200
+    low = -200
+    start = -200
+    end = -200
+
+    station = ""
+    day = ""
+    station_day = ""
+
+    writer.write("Station Name,Date,Min Temp,Max Temp,First Temp,Last Temp\n")
+
+    for line in reader:
+        fields = line.split(",") 
+        st = fields[0]
+        
+        timefields = fields[1].split()
+        dt = timefields[0]
+        hour = timefields[1]
+        ampm = timefields[2]
+
+        temp = float(fields[2])
+        st_day = st + "," + dt
+
+        if station_day != st_day:
+            if end > -200:
+                statline = ",".join([station, day, str(low), str(high), str(start), str(end)])
+                writer.write(statline + "\n")
+
+            station = st
+            day = dt
+            station_day = st_day
+
+            end = temp
+            start = temp
+            high = temp
+            low = temp
+
+        else:
+            start = temp
+            if temp > high:
+                high = temp
+            if temp < low:
+                low = temp
+
+    if end > -200:
+        statline = ",".join([station, day, str(low), str(high), str(start), str(end)])
+        writer.write(statline + "\n")        
+
+    #Foster Weather Station,01/01/2016 11:00:00 PM,69.0        
     writer.write(f"Saw {len(reader.readlines())} lines" + "\n")
--- test/weather_test.py
+++ test/weather_test.py
@@ -1,9 +1,26 @@
 from interview import weather
 import io
+import sys
 
 
 def test_replace_me():
-    reader = io.StringIO("Line One\nLine Two\n")
-    writer = io.StringIO()
+    testlines = "Foster Weather Station,01/09/2016 09:00:00 PM,69.0\n"
+    testlines += "Foster Weather Station,01/09/2016 08:00:00 PM,77.0\n"
+    testlines += "Foster Weather Station,01/09/2016 07:00:00 PM,62.0\n"
+    testlines += "Foster Weather Station,01/09/2016 06:00:00 PM,70.0\n"
+
+    testlines += "Buoy Station,01/09/2016 09:00:00 PM,69\n"
+    testlines += "Buoy Station,01/09/2016 08:00:00 PM,77\n"
+    testlines += "Buoy Station,01/09/2016 07:00:00 PM,62\n"
+    testlines += "Buoy Station,01/09/2016 06:00:00 PM,70\n"
+
+    testlines += "Buoy Station,01/08/2016 08:00:00 PM,55.0\n"
+
+    testlines += "Buoy Station,01/07/2016 08:00:00 PM,41.0\n"
+    testlines += "Buoy Station,01/07/2016 07:00:00 PM,42.0\n"
+
+    reader = io.StringIO(testlines)
+    #writer = io.StringIO()
+    writer = sys.stdout
     weather.process_csv(reader, writer)
-    assert writer.getvalue() == "Saw 2 lines\n"
+    #assert writer.getvalue() == "Saw 2 lines\n"
